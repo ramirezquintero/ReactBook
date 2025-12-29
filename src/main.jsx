a99ee037e7fb65daa22d971a334d6ff70c7c129c
Adan Ramirez <adan.alejandro.ramirez107@gmail.com>
2025-12-29 13:48:01 -0600

    Added the test for case sensitive HTML tags

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,9 @@ import * as ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <div>
-    <button />
-    <code />
-    <input />
-    <label />
-    <p />
-    <pre />
-    <select />
-    <table />
-    <ul />
-  </div>
+  <button title="My Button" foo="bar">
+    My Button
+    </button>
 );
+
+root.render(<Button />);
